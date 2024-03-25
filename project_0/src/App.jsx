a33c1c66b7a65d0f18c374c4290788f285050d4d
Hemant Ayuj <hemant.ayuj@gmail.com>
2024-03-25 12:26:32 +0530

    added text area for user input

--- project_0/src/App.jsx
+++ project_0/src/App.jsx
@@ -1,15 +1,27 @@
-import './App.css'
-import { Button } from './components/ui/button'
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Textarea } from "./components/ui/textarea";
 
 function App() {
-
   return (
     <>
-      <Button className="font-mono rounded-xl">
-      first button 
-      </Button>
+      <Textarea
+          className="bg-blue-100 h-50 border-purple-600"
+        >
+
+        </Textarea>
+      <div className="grid justify-center items-center">
+        
+        <Button 
+        className="font-mono rounded-xl text-black-600 bg-blue-600 m-10 animate-bounce w-64 "
+        onClick={() => alert("Hello World")}
+        >
+          Submit
+        </Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
