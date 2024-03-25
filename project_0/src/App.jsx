b968da9114e43d4ccac1320f70ac68dfaffa5dd1
Hemant Ayuj <hemant.ayuj@gmail.com>
2024-03-25 13:48:48 +0530

    gemini api used but getting some error

--- project_0/src/App.jsx
+++ project_0/src/App.jsx
@@ -1,21 +1,32 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
 import { Textarea } from "./components/ui/textarea";
+import React, { useState } from "react";
+import TextResponseModel from "./components/TextResponseModel";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(TextResponseModel(userInput));
+  };
+
   return (
     <>
+      <h1>Enter your Query here</h1>
       <Textarea
-          className="bg-blue-100 h-50 border-purple-600"
-        >
-
-        </Textarea>
+        className="bg-blue-100 h-50 border-purple-600 mt-5"
+        value={userInput}
+        onChange={handleInputChange}
+      />
       <div className="grid justify-center items-center">
-        
-        <Button 
-        className="font-mono rounded-xl text-black-600 bg-blue-600 m-10 animate-bounce w-64 "
-        onClick={() => alert("Hello World")}
+        <Button
+          className="font-mono rounded-xl text-black-600 bg-blue-600 m-10 animate-bounce w-64"
+          onClick={handleSubmit}
         >
           Submit
         </Button>
---
+++ project_0/src/components/TextResponseModel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const api_key = import.meta.env.VITE_GEMINI_API_KEY;
+
+const genAI = new GoogleGenerativeAI(api_key);
+
+
+const TextResponseModel = async (prompt) => {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const result = await model.generateContent(prompt);
+  const res = result.response;
+  const text = res.text();
+  return text;
+}
+
+export default TextResponseModel;
